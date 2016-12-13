e4d53a0ef829376b343a2f9c492aec864d0ad944
jeremybyron <jeremybyron@yahoo.com>
2016-12-14 02:59:34 +0800

    Update speak_text.py

--- speak_text.py
+++ speak_text.py
@@ -30,8 +30,8 @@ from subprocess import call
 
 
 cmd_beg= 'espeak '
-cmd_end= ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
-cmd_out= '--stdout > /home/pi/Desktop/Text.wav ' # To store the voice file
+cmd_end= 'aplay Text.wav  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
+cmd_out= '--stdout > Text.wav ' # To store the voice file
 
 text = input("Enter the Text: ")
 print(text)
@@ -40,8 +40,5 @@ print(text)
 text = text.replace(' ', '_')
 
 #Calls the Espeak TTS Engine to read aloud a Text
-call([cmd_beg+cmd_out+text+cmd_end], shell=True)
-
-
-
-
+call([cmd_beg+cmd_out+text], shell=True)
+call([cmd_end], shell=True)
